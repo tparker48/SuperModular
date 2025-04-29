d7796367b5e2c982d9c8f3603544b449b50d28c4
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2025-04-29 13:40:33 -0700

    update tests

--- Source/OscillatorProcessorTest.h
+++ Source/OscillatorProcessorTest.h
@@ -23,24 +23,24 @@ public:
         OscillatorProcessor osc(1);
         osc.prepareToPlay(3, 0);
         osc.setHz(1.0);
-        CVOutputJack jout;
-        jout.write(0.0);
-        osc.setCVInputJack(1, &jout);
 
-        osc.processSample();
-        osc.processSample();
-        osc.processSample();
-
+        osc.processSample(); // 0
+        osc.processSample(); // 1
+        osc.processSample(); // 2
+        osc.processSample(); // 3 (wraps to 0)
+        
         auto sinOut = *(osc.getCVOutputJack(0)->getPtr());
         auto expSinOut = sin(0.0);
         expect(abs(expSinOut - sinOut) < 1e-10);
 
         OscillatorProcessor osc2(1);
-        osc2.prepareToPlay(100, 0);
-        osc2.setHz(1.0);
-        for (int i = 0; i < 100; i++) {
+        osc2.prepareToPlay(1000, 0);
+        osc2.setHz(100.0);
+
+        for (int i = 0; i <= 1000; i++) {
             osc2.processSample();
         }
+     
         sinOut = *(osc2.getCVOutputJack(0)->getPtr());
         expSinOut = sin(0.0);
         expect(abs(expSinOut - sinOut) < 1e-10);
