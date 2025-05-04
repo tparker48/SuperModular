1194d2a5b81c286773c1a0f37bfe9e524ee2401a
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2025-05-04 02:11:03 -0700

    beautify sequencer

--- Source/SequencerUI.h
+++ Source/SequencerUI.h
@@ -16,7 +16,7 @@
 
 class SequencerUI : public ModuleUI, public Slider::Listener {
 public:
-    static const int hp = 4;
+    static const int hp = 3;
 
     SequencerUI(int id, ModuleGrid* mg, PatchCableManager* cm, SharedStateWriter* stateWriter) :
         ModuleUI(id, mg, cm, stateWriter, 1, 1) {
@@ -26,6 +26,7 @@ public:
             stepSliders[i].setRange(-1.0, 1.0);
             stepSliders[i].setTitle(std::to_string(i));
             stepSliders[i].setName(std::to_string(i));
+            stepSliders[i].setColours(knobCol, dotCol);
             addAndMakeVisible(stepSliders[i]);
         }
         rateSlider.setValue(0.5);
@@ -33,13 +34,18 @@ public:
         rateSlider.setSkewFactor(0.3);
         rateSlider.setTitle("rate");
         rateSlider.setName("rate");
+        rateSlider.setColours(knobCol, dotCol);
         addAndMakeVisible(rateSlider);
 
         numStepsSlider.setValue(MAX_STEPS);
         numStepsSlider.setRange(1, MAX_STEPS, 1);
         numStepsSlider.setTitle("steps");
         numStepsSlider.setName("steps");
+        numStepsSlider.setColours(knobCol, dotCol);
         addAndMakeVisible(numStepsSlider);
+
+        getCvInputJack(0)->setJackColour(cvCol);
+        getCvOutputJack(0)->setJackColour(cvCol);
     }
 
     void sliderValueChanged(juce::Slider* slider) override
@@ -88,43 +94,54 @@ public:
     }
 
     void paintModule(Graphics& g) override {
-        g.setColour(Colour(228, 224, 255));
+        g.setColour(bgCol);
         g.fillAll();
+
+        paintComponentLabel(g, getCvInputJack(0), "sync", TOP, getHeight() * 0.02, textCol);
+        paintComponentLabel(g, getCvOutputJack(0), "out", TOP, getHeight() * 0.02, textCol);
+
+        for (int i = 0; i < MAX_STEPS; i++) {
+            paintComponentLabel(g, &stepSliders[i], std::to_string(i), TOP, getHeight() * 0.02, textCol);
+
+        }
+
+        paintComponentLabel(g, &rateSlider, "rate", TOP, getHeight() * 0.02, textCol);
+        paintComponentLabel(g, &numStepsSlider, "steps", TOP, getHeight() * 0.02, textCol);
     }
 
     void resized() override {
         int margin = getHeight() * 0.05;
-        int spacing = getHeight() * 0.05;
-        int steKnobSize = getWidth() * 0.20;
+        int marginY = getHeight() * 0.3;
+        int spacing = getHeight() * 0.17;
+        int steKnobSize = getWidth() * 0.22;
         int knobSize = getWidth() * 0.25;
 
-        rateSlider.setBounds(margin, 5*margin, knobSize, knobSize);
-        numStepsSlider.setBounds(margin, 5*margin + knobSize + spacing, knobSize, knobSize);
+        rateSlider.setBounds(margin, margin, knobSize, knobSize);
+        numStepsSlider.setBounds(margin + knobSize + spacing, margin, knobSize, knobSize);
 
         for (int i = 0; i < MAX_STEPS / 2; i++) {
-            stepSliders[i].setBounds(
-                6 * margin, 
-                margin + ((steKnobSize +spacing) * i),
-                steKnobSize,
-                steKnobSize
-            );
+            stepSliders[i].setSize(steKnobSize, steKnobSize);
+            stepSliders[i].setCentrePosition(getWidth() / 2 - getWidth()*0.12, marginY + spacing * i);
         }
         for (int i = MAX_STEPS / 2; i < MAX_STEPS; i++) {
-            stepSliders[i].setBounds(
-                6 * margin + spacing + steKnobSize,
-                margin + ((steKnobSize + spacing) * (i - 4)),
-                steKnobSize,
-                steKnobSize
-            );
+            stepSliders[i].setSize(steKnobSize, steKnobSize);
+            stepSliders[i].setCentrePosition(getWidth() / 2 + getWidth() * 0.12, marginY + spacing * (i-4));
         }
 
-        getCvOutputJack(0)->setBounds(getWidth() - margin - 25, getHeight() - margin - 25, 25, 25);
-        getCvInputJack(0)->setSize(25,25);
-        getCvInputJack(0)->setCentrePosition(getWidth() - margin, getHeight() - margin);
+        getCvOutputJack(0)->setSize(25, 25);
+        getCvOutputJack(0)->setCentrePosition(getWidth() - margin - 12.5, getHeight() * 0.95);
+        getCvInputJack(0)->setSize(25, 25);
+        getCvInputJack(0)->setCentrePosition(0 + margin + 12.5, getHeight() * 0.95);
     }
 
 private:
     static const  int MAX_STEPS = 8;
     Dial stepSliders[MAX_STEPS];
     Dial rateSlider, numStepsSlider;
+
+    Colour bgCol = Colour(0xFFBAD9B5);
+    Colour textCol = Colour(0xFF393424);
+    Colour knobCol = Colour(0xFF393424);
+    Colour dotCol = Colour(0xFFEFF7CF);
+    Colour cvCol = Colour(0xFF393424);
 };
--- Source/SplitterUI.h
+++ Source/SplitterUI.h
@@ -19,25 +19,52 @@ public:
 
     SplitterUI(int id, ModuleGrid* mg, PatchCableManager* cm, SharedStateWriter* stateWriter) :
         ModuleUI(id, mg, cm, stateWriter, 1, 5) {
+
+        getCvOutputJack(0)->setJackColour(cvCol);
+        getCvOutputJack(1)->setJackColour(cvCol);
+        getCvOutputJack(2)->setJackColour(cvCol);
+        getCvOutputJack(3)->setJackColour(cvCol);
+        getCvOutputJack(4)->setJackColour(cvCol);
+        getCvInputJack(0)->setJackColour(cvCol);
     }
 
     void paintModule(Graphics& g) override {
-        g.setColour(Colour(250, 94, 80));
+        g.setColour(bgCol);
         g.fillAll();
+
+        paintComponentLabel(g, getCvInputJack(0),  "in",    TOP, getHeight() * 0.03, textCol);
+        paintComponentLabel(g, getCvOutputJack(0), "out 1", TOP, getHeight() * 0.03, textCol);
+        paintComponentLabel(g, getCvOutputJack(1), "out 2", TOP, getHeight() * 0.03, textCol);
+        paintComponentLabel(g, getCvOutputJack(2), "out 3", TOP, getHeight() * 0.03, textCol);
+        paintComponentLabel(g, getCvOutputJack(3), "out 4", TOP, getHeight() * 0.03, textCol);
+        paintComponentLabel(g, getCvOutputJack(4), "out 5", TOP, getHeight() * 0.03, textCol);
     }
 
     void resized() override {
-        auto margin = getWidth() * 0.10;
-        auto cvY = getHeight() * 0.3;
-        auto paddingY = getHeight() * 0.2;
+        auto cvY = getHeight() * 0.2;
+        auto paddingY = getHeight() * 0.15;
 
-        getCvInputJack(0)->setBounds(Rectangle<int>(margin, cvY + 2 * paddingY, 25, 25));
+        getCvOutputJack(0)->setSize(25, 25);
+        getCvOutputJack(0)->setCentrePosition(getWidth() / 2, getHeight() * 0.15);
 
-        getCvOutputJack(0)->setBounds(Rectangle<int>(getWidth()- margin - 25, cvY + 0*paddingY, 25, 25));
-        getCvOutputJack(1)->setBounds(Rectangle<int>(getWidth()- margin - 25, cvY + 1*paddingY, 25, 25));
-        getCvOutputJack(2)->setBounds(Rectangle<int>(getWidth()- margin - 25, cvY + 2*paddingY, 25, 25));
-        getCvOutputJack(3)->setBounds(Rectangle<int>(getWidth()- margin - 25, cvY + 3*paddingY, 25, 25));
-        getCvOutputJack(4)->setBounds(Rectangle<int>(getWidth()- margin - 25, cvY + 4*paddingY, 25, 25));
-        
+        getCvOutputJack(1)->setSize(25, 25);
+        getCvOutputJack(1)->setCentrePosition(getWidth() / 2, getHeight() * 0.15 + 1 * paddingY);
+
+        getCvOutputJack(2)->setSize(25, 25);
+        getCvOutputJack(2)->setCentrePosition(getWidth() / 2, getHeight() * 0.15 + 2 * paddingY);
+
+        getCvOutputJack(3)->setSize(25, 25);
+        getCvOutputJack(3)->setCentrePosition(getWidth() / 2, getHeight() * 0.15 + 3 * paddingY);
+
+        getCvOutputJack(4)->setSize(25, 25);
+        getCvOutputJack(4)->setCentrePosition(getWidth() / 2, getHeight() * 0.15 + 4 * paddingY);
+
+        getCvInputJack(0)->setSize(25, 25);
+        getCvInputJack(0)->setCentrePosition(getWidth() / 2, getHeight()*0.98 - 12.5);
     }
+    
+private:
+    Colour bgCol = Colour(0xFFE0DDCF);
+    Colour cvCol = Colour(0xFF3B252C);
+    Colour textCol = Colour(0xFF3B252C);
 };
